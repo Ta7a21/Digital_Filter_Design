c3aba02e73c42162c9fdbf9b96a744c02c18f34c
Ahmed Mohamed Abdelfattah <ahmed.hassaan99@eng-st.cu.edu.eg>
2021-06-15 01:42:00 +0200

    drag partial complete

--- graph.js
+++ graph.js
@@ -1,10 +1,18 @@
 
 //window.onload = function() {draw()}
 var pointSize = 3, zeros = [], poles = [], canvas = document.getElementById('oy');
-var context,
-  centerX,
+var centerX,
   radius,
   centerY;
+var WIDTH = canvas.width;
+var HEIGHT = canvas.height;
+var dragok = false;
+var startX;
+var startY;
+
+canvas.onmousedown = myDown;
+canvas.onmouseup = myUp;
+canvas.onmousemove = myMove;
 function draw() {
   var ctx = canvas.getContext('2d');
   radius = 200;
@@ -35,11 +43,10 @@ function getPosition(event) {
   var x = event.clientX - rect.left;
   var y = event.clientY - rect.top;
 
-  if (Math.pow((x - centerX), 2) + Math.pow((y - centerY), 2) <= Math.pow(radius, 2))
-  {
-    flag = del(x,y);
-    if(!flag)
-    drawCoordinates(x, y, document.getElementById('zero').checked,);
+  if (Math.pow((x - centerX), 2) + Math.pow((y - centerY), 2) <= Math.pow(radius, 2)) {
+    // flag = del(x, y);
+    // if (!flag)
+    drawCoordinates(x, y, document.getElementById('zero').checked);
 
   }
 }
@@ -50,14 +57,14 @@ function drawCoordinates(x, y, flag) {
 
   ctx.beginPath();
   if (flag) {
-    zeros.push([x, y]);
+    zeros.push({ x, y, isDragging: false });
     ctx.fillStyle = "#ffffff"; // Red color
 
     ctx.arc(x, y, pointSize, 0, Math.PI * 2, true);
     ctx.fill();
   }
   else {
-    poles.push([x, y]);
+    poles.push({ x, y, isDragging: false });
     ctx.moveTo(x - 5, y - 5);
     ctx.lineTo(x + 5, y + 5);
 
@@ -73,77 +80,70 @@ var inRange = function (num, start, end) {
 
   // If no end number, use start as end
   if (!end) {
-      end = start;
-      start = 0;
+    end = start;
+    start = 0;
   }
 
   return num >= start && num <= end;
 
 };
 
-function del(x,y)
-{
+function del(x, y) {
   var flag = false
- for (i=0; i<zeros.length; i++)
- {
- xmax = zeros[i][0] + 5
- xmin = zeros[i][0] - 5
- ymax = zeros[i][1] + 5
- ymin = zeros[i][1] - 5
- if(inRange(x,xmin,xmax))
- {
-  if(inRange(y,ymin,ymax)){
-    zeros.splice(i,1)
-    flag = true
-  
-  }
-}
-}
+  for (i = 0; i < zeros.length; i++) {
+    xmax = zeros[i].x + 5
+    xmin = zeros[i].x - 5
+    ymax = zeros[i].y + 5
+    ymin = zeros[i].y - 5
+    if (inRange(x, xmin, xmax)) {
+      if (inRange(y, ymin, ymax)) {
+        zeros.splice(i, 1)
+        flag = true
 
-   for (i=0; i<poles.length; i++)
- {
- xmax = poles[i][0] + 5
- xmin = poles[i][0] - 5
- ymax = poles[i][1] + 5
- ymin = poles[i][1] - 5
- if(inRange(x,xmin,xmax))
- {
-  if(inRange(y,ymin,ymax)){
-    poles.splice(i,1) 
-    flag = true
-  
-  }
-}
- }
- 
- if(flag){  
- draw();
- for (let i = 0; i < poles.length; i += 1)
-  {
-   var ctx = canvas.getContext("2d");
-   ctx.beginPath();
-   ctx.moveTo(poles[i][0] - 5, poles[i][1] - 5);
-   ctx.lineTo(poles[i][0] + 5, poles[i][1] + 5);
-
-   ctx.moveTo(poles[i][0] + 5, poles[i][1] - 5);
-   ctx.lineTo(poles[i][0] - 5, poles[i][1] + 5);
-   ctx.strokeStyle = "#ffffff";
-   ctx.stroke();
-   ctx.closePath();
+      }
+    }
   }
 
-for (let i = 0; i < zeros.length; i += 1)
-{
- var ctx = canvas.getContext("2d");
- ctx.beginPath();
- ctx.fillStyle = "#ffffff"; // Red color
+  for (i = 0; i < poles.length; i++) {
+    xmax = poles[i].x + 5
+    xmin = poles[i].x - 5
+    ymax = poles[i].y + 5
+    ymin = poles[i].y - 5
+    if (inRange(x, xmin, xmax)) {
+      if (inRange(y, ymin, ymax)) {
+        poles.splice(i, 1)
+        flag = true
 
- ctx.arc(zeros[i][0], zeros[i][1], pointSize, 0, Math.PI * 2, true);
- ctx.fill();
- ctx.closePath();
-} 
- }
- return flag
+      }
+    }
+  }
+
+  if (flag) {
+    draw();
+    for (let i = 0; i < poles.length; i += 1) {
+      var ctx = canvas.getContext("2d");
+      ctx.beginPath();
+      ctx.moveTo(poles[i].x - 5, poles[i].y - 5);
+      ctx.lineTo(poles[i].x + 5, poles[i].y + 5);
+
+      ctx.moveTo(poles[i].x + 5, poles[i].y - 5);
+      ctx.lineTo(poles[i].x - 5, poles[i].y + 5);
+      ctx.strokeStyle = "#ffffff";
+      ctx.stroke();
+      ctx.closePath();
+    }
+
+    for (let i = 0; i < zeros.length; i += 1) {
+      var ctx = canvas.getContext("2d");
+      ctx.beginPath();
+      ctx.fillStyle = "#ffffff"; // Red color
+
+      ctx.arc(zeros[i].x, zeros[i].y, pointSize, 0, Math.PI * 2, true);
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+  return flag
 }
 
 function clearCanvas(toClear) {
@@ -153,11 +153,11 @@ function clearCanvas(toClear) {
     for (let i = 0; i < poles.length; i += 1) {
       var ctx = canvas.getContext("2d");
       ctx.beginPath();
-      ctx.moveTo(poles[i][0] - 5, poles[i][1] - 5);
-      ctx.lineTo(poles[i][0] + 5, poles[i][1] + 5);
+      ctx.moveTo(poles[i].x - 5, poles[i].y - 5);
+      ctx.lineTo(poles[i].x + 5, poles[i].y + 5);
 
-      ctx.moveTo(poles[i][0] + 5, poles[i][1] - 5);
-      ctx.lineTo(poles[i][0] - 5, poles[i][1] + 5);
+      ctx.moveTo(poles[i].x + 5, poles[i].y - 5);
+      ctx.lineTo(poles[i].x - 5, poles[i].y + 5);
       ctx.strokeStyle = "#ffffff";
       ctx.stroke();
       ctx.closePath();
@@ -171,7 +171,7 @@ function clearCanvas(toClear) {
       ctx.beginPath();
       ctx.fillStyle = "#ffffff"; // Red color
 
-      ctx.arc(zeros[i][0], zeros[i][1], pointSize, 0, Math.PI * 2, true);
+      ctx.arc(zeros[i].x, zeros[i].y, pointSize, 0, Math.PI * 2, true);
       ctx.fill();
       ctx.closePath();
     }
@@ -182,6 +182,117 @@ function clearCanvas(toClear) {
     poles = []
   }
 }
+
+function myDown(e) {
+  // console.log(1);
+  // tell the browser we're handling this mouse event
+  e.preventDefault();
+  e.stopPropagation();
+
+  // get the current mouse position
+  var rect = canvas.getBoundingClientRect();
+  var x = e.clientX - rect.left;
+  var y = e.clientY - rect.top;
+
+  // test each shape to see if mouse is inside
+  dragok = false;
+  for (var i = 0; i < zeros.length; i++) {
+    // test if the mouse is inside this circle
+    xmax = zeros[i].x + 1
+    xmin = zeros[i].x - 1
+    ymax = zeros[i].y + 1
+    ymin = zeros[i].y - 1
+    if (inRange(x, xmin, xmax)) {
+      if (inRange(y, ymin, ymax)) {
+        dragok = true;
+        zeros[i].isDragging = true;
+      }
+
+    }
+    // save the current mouse position
+    startX = x;
+    startY = y;
+  }
+}
+
+// handle mouseup events
+function myUp(e) {
+  // console.log(2);
+  // tell the browser we're handling this mouse event
+  e.preventDefault();
+  e.stopPropagation();
+
+  // clear all the dragging flags
+  dragok = false;
+  for (var i = 0; i < zeros.length; i++) {
+    zeros[i].isDragging = false;
+  }
+}
+
+
+// handle mouse moves
+function myMove(e) {
+  // if we're dragging anything...
+  if (dragok) {
+    console.log(3);
+
+    // tell the browser we're handling this mouse event
+    e.preventDefault();
+    e.stopPropagation();
+
+    // get the current mouse position
+    var rect = canvas.getBoundingClientRect();
+    var mx = e.clientX - rect.left;
+    var my = e.clientY - rect.top;
+
+    // calculate the distance the mouse has moved
+    // since the last mousemove
+    var dx = mx - startX;
+    var dy = my - startY;
+
+    // move each rect that isDragging 
+    // by the distance the mouse has moved
+    // since the last mousemove
+    for (var i = 0; i < zeros.length; i++) {
+      var s = zeros[i];
+      if (s.isDragging) {
+        s.x += dx;
+        s.y += dy;
+      }
+    }
+
+    // redraw the scene with the new rect positions
+    draw();
+    for (let i = 0; i < poles.length; i += 1) {
+      var ctx = canvas.getContext("2d");
+      ctx.beginPath();
+      ctx.moveTo(poles[i].x - 5, poles[i].y - 5);
+      ctx.lineTo(poles[i].x + 5, poles[i].y + 5);
+
+      ctx.moveTo(poles[i].x + 5, poles[i].y - 5);
+      ctx.lineTo(poles[i].x - 5, poles[i].y + 5);
+      ctx.strokeStyle = "#ffffff";
+      ctx.stroke();
+      ctx.closePath();
+    }
+
+    for (let i = 0; i < zeros.length; i += 1) {
+      var ctx = canvas.getContext("2d");
+      ctx.beginPath();
+      ctx.fillStyle = "#ffffff"; // Red color
+
+      ctx.arc(zeros[i].x, zeros[i].y, pointSize, 0, Math.PI * 2, true);
+      ctx.fill();
+      ctx.closePath();
+    }
+
+    // reset the starting mouse position for the next mousemove
+    startX = mx;
+    startY = my;
+
+  }
+}
+
 WebFontConfig = {
   google: { families: ['Open+Sans+Condensed:300:latin'] }
 };
